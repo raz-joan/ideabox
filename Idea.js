7490df1f234fc824b6903b8dee6dfe5edfe3e759
Joan Rasmussen <raz.joan@gmail.com>
2021-09-15 15:07:27 -0600

    Build idea Class with four empty methods

---
+++ Idea.js
@@ -0,0 +1,20 @@
+class Idea {
+  constructor(title, body, star) {
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.star = star;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateIdea() {
+    
+  }
+}
