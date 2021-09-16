1b7d56361524586b92366bdc6c85b5e785b4477a
Joan Rasmussen <raz.joan@gmail.com>
2021-09-15 18:53:14 -0600

    Add saveToArray and showIdeaCards functions

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 var titleInput = document.querySelector('.js-title-input');
 var bodyInput = document.querySelector('.js-body-input');
 var saveButton = document.querySelector('.js-save-button');
+var cardContainer = document.querySelector('.js-card-section');
 
 // other variables go below
 var ideas = [];
@@ -11,21 +12,36 @@ var ideas = [];
 saveButton.addEventListener('click', verifyInput);
 
 // functions and event handler go below
+function showIdeaCards() {
+  cardContainer.innerHTML = "";
+  for (var i = 0; i < ideas.length; i++) {
+    cardContainer.innerHTML += `<article class="card-article">
+        <div class="card-top-bar">
+          <img class ="white-star-icon" src="./Assets/star.svg">
+          <img class="white-delete-icon" src="./Assets/delete.svg">
+        </div>
+        <div class="card-body">
+          <h3>${ideas[i].title}</h3>
+          <p>${ideas[i].body}</p>
+        </div>
+        <div class="card-bottom-bar">
+          <img class="comment-icon" src="./Assets/comment.svg">
+          <p class="bottom-bar-comment">Comment</p>
+        </div>
+       </article>`;
+  }
+};
+
+function saveToArray(title, body) {
+  var ideaCard = new Idea(title, body);
+  ideas.push(ideaCard);
+  showIdeaCards();
+};
+
 function verifyInput() {
-  // check the title input for string
-  // check the body input for string
-  // if not both are true, alert!
-  // create an idea instance
-  // then push this instance into the ideas array
-  // idea instance should show on card in bottom section
   if (titleInput.value === "" || bodyInput.value === "") {
    window.alert('Wait! you must input a title and body');
   } else {
     saveToArray(titleInput.value, bodyInput.value);
   }
-}
-
-function saveToArray(title, body) {
-  var ideaCard = new Idea(title, body);
-  ideas.push(ideaCard);
-}
+};
