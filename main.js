091ff86c9f9f60fccbec7a99d09891c1de8d6454
Joan Rasmussen <raz.joan@gmail.com>
2021-09-15 18:12:20 -0600

    Add verifyInputs and saveToArray functions

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// psuedo for first as a user
-// create a variable for title
-// create a variable for body
-// create a variable for save button
-// add event listener to the save button
-// create an instance from the inputs IF there are inputs that are strings
 
 // querySelectors go below
 var titleInput = document.querySelector('.js-title-input');
@@ -11,8 +5,27 @@ var bodyInput = document.querySelector('.js-body-input');
 var saveButton = document.querySelector('.js-save-button');
 
 // other variables go below
+var ideas = [];
 
 // eventListeners go below
-saveButton.addEventListener('click', saveIdeaInput);
+saveButton.addEventListener('click', verifyInput);
 
 // functions and event handler go below
+function verifyInput() {
+  // check the title input for string
+  // check the body input for string
+  // if not both are true, alert!
+  // create an idea instance
+  // then push this instance into the ideas array
+  // idea instance should show on card in bottom section
+  if (titleInput.value === "" || bodyInput.value === "") {
+   window.alert('Wait! you must input a title and body');
+  } else {
+    saveToArray(titleInput.value, bodyInput.value);
+  }
+}
+
+function saveToArray(title, body) {
+  var ideaCard = new Idea(title, body);
+  ideas.push(ideaCard);
+}
